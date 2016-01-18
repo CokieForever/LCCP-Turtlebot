9bf24cd7ae6653d642c2ad9276ff939f2789dd77
Alexandros Sivris <alexandros.sivris@tum.de>
2016-01-18 23:22:29 +0100

    new callback function for received markers

--- project/src/stopper_package/src/stopper.cpp
+++ project/src/stopper_package/src/stopper.cpp
@@ -4,103 +4,152 @@
 //Constructor
 Stopper::Stopper()
 {
-	keepMoving = true;
-    commandPub = node.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 10);
-    laserSub = node.subscribe("/scan", 1, &Stopper::scanCallback, this);
-    markerId = node.subscribe("/int8_marker_ids", 10, &Stopper::markerCallback, this);
-    bumper = node.subscribe("mobile_base/events/bumper", 20, &Stopper::bumperCallback, this);
-	nextId = 0;
+  keepMoving = true;
+
+  //advertisements
+  commandPub = node.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 10);
+
+  //subscriptions
+  laserSub = node.subscribe("/scan", 1, &Stopper::scanCallback, this);
+  markerId = node.subscribe("/int8_marker_ids", 10, &Stopper::markerCallback, this);
+  bumper = node.subscribe("mobile_base/events/bumper", 20, &Stopper::bumperCallback, this);
+  m_markerSub = node.subscribe("/<MARKER TOPIC HERE>", 10, &Stopper::markerDetectCallback, this);
+  nextId = 0;
+}
+//Callback function that is invoked when a marker msg arrives - Alex
+void markerDetectCallback(const detect_marker::Markers::ConstPtr& marker_msg)
+{
+  //map the coordiante to the center
+  float f_Xm = marker_msg->x - 320;
+  enum e_Direction { left , right};
+
+  //adjust robot, so the marker actually is in the center
+  if (f_Xm < 0)
+  {
+    //rotate bot to the right
+    e_Direction = right;
+    m_angularVelocity = 0.1;
+    rotateTurtlebot();
+  }
+  else
+    if (e_Direction != left) m_angularVelocity = 0;
+
+  if (f_Xm > 0)
+  {
+    //rotate bot to the left
+    e_Direction = left;
+    m_angularVelocity = -0.1;
+    rotateTurtlebot();
+  }
+  else
+    if (e_Direction != right) m_angularVelocity = 0;
+}
+
+//rotation of the turtlebot - Alex
+void rotateTurtlebot()
+{
+  ros::Rate rate(10);
+  while(ros::ok())
+  {
+    geometry_msgs::Twist vel_msg;
+    vel_msg.angular.z = m_angularVelocity;
+    vel_msg.linear.x = 0.2;
+    publish(vel_msg);
+    // jump into a callback function, in case there is a message coming
+    ros::spinOnce();
+    rate.sleep();
+  }
 }
 
 //Method for a forward movement of the turtlebot
 void Stopper::moveForward()
 {
-	geometry_msgs::Twist msg;
-	msg.linear.x = FORWARD_SPEED_MPS;
-	commandPub.publish(msg);
+  geometry_msgs::Twist msg;
+  msg.linear.x = FORWARD_SPEED_MPS;
+  commandPub.publish(msg);
 }
 
 
 //Will be called if there is an obstacle or a bumper was launched
 void Stopper::rotate()
 {
-    geometry_msgs::Twist msg;
-    msg.angular.z = 0.785398;
-    commandPub.publish(msg);
+  geometry_msgs::Twist msg;
+  msg.angular.z = 0.785398;
+  commandPub.publish(msg);
 }
 
 //Callback of the /scan topic. Checks wether an obstacle disturbs forward movement
 void Stopper::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
-    int minIndex = ceil((MIN_SCAN_ANGLE_RAD - scan->angle_min) / scan->angle_increment);
-    int maxIndex = floor((MAX_SCAN_ANGLE_RAD - scan->angle_min) / scan->angle_increment);
-	
-    float closestRange = scan->ranges[minIndex];
+  int minIndex = ceil((MIN_SCAN_ANGLE_RAD - scan->angle_min) / scan->angle_increment);
+  int maxIndex = floor((MAX_SCAN_ANGLE_RAD - scan->angle_min) / scan->angle_increment);
 
-    for(int currIndex = minIndex+1; currIndex<=maxIndex; currIndex++)
-	{
-        if(scan->ranges[currIndex] < closestRange)
-		{
-            closestRange =	scan->ranges[currIndex];
-		}
-    }
+  float closestRange = scan->ranges[minIndex];
 
-	if(closestRange<MIN_PROXIMITY_RANGE_M)	
-	{
-        ROS_INFO("Obstacle!");
-		keepMoving=false;
-        rotate();
-	}
-    else
+  for(int currIndex = minIndex+1; currIndex<=maxIndex; currIndex++)
+  {
+    if(scan->ranges[currIndex] < closestRange)
     {
-        keepMoving = true;
+      closestRange =	scan->ranges[currIndex];
     }
+  }
+
+  if(closestRange<MIN_PROXIMITY_RANGE_M)
+  {
+    ROS_INFO("Obstacle!");
+    keepMoving=false;
+    rotate();
+  }
+  else
+  {
+    keepMoving = true;
+  }
 }
 
 //Method which will be called from run_stopper.cpp
 void Stopper::startMoving()
 {
-    ros::Rate rate(10);
-    ROS_INFO("Start moving");
+  ros::Rate rate(10);
+  ROS_INFO("Start moving");
 
-    while(ros::ok() && keepMoving)
-    {
-        moveForward();
-        ros::spinOnce();
-        rate.sleep();
-    }
-    while(!keepMoving)
-    {
-        rotate();
-        ros::spinOnce();
-    }
+  while(ros::ok() && keepMoving)
+  {
+    moveForward();
+    ros::spinOnce();
+    rate.sleep();
+  }
+  while(!keepMoving)
+  {
+    rotate();
+    ros::spinOnce();
+  }
 }
 
 //Callback function for incoming Marker msgs. Checks the detected MarkerIDs and compare it to the next wanted ID.
 void Stopper::markerCallback(const detect_marker::Markers::ConstPtr& markers_msg)
 {
-	int highestMarkerId = 0;
-	
-    for(int i=0; i<8; i++)
-	{
-        if((markers_msg->marker[i] >= highestMarkerId) && (markers_msg->marker[i]!=8))
-        {
-            ROS_INFO("Marker content: %d", markers_msg->marker[i] );
-            highestMarkerId = markers_msg->marker[i];
-        }
-	}
-	
-    if(highestMarkerId == nextId && !keepMoving)
+  int highestMarkerId = 0;
+
+  for(int i=0; i<8; i++)
+  {
+    if((markers_msg->marker[i] >= highestMarkerId) && (markers_msg->marker[i]!=8))
     {
-        nextId =+ 1;
-        ROS_INFO("next ID is now %d", nextId);
+      ROS_INFO("Marker content: %d", markers_msg->marker[i] );
+      highestMarkerId = markers_msg->marker[i];
     }
+  }
+
+  if(highestMarkerId == nextId && !keepMoving)
+  {
+    nextId =+ 1;
+    ROS_INFO("next ID is now %d", nextId);
+  }
 }		
 
 //Callback function for the bumper event. Stops the forward movement and iniates an rotation
 void Stopper::bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& bumper_msg)
 {
-    ROS_INFO("You just get bumped!");
-    keepMoving=false;
-    rotate();
+  ROS_INFO("You just get bumped!");
+  keepMoving=false;
+  rotate();
 }
--- project/src/stopper_package/src/stopper.hpp
+++ project/src/stopper_package/src/stopper.hpp
@@ -4,28 +4,35 @@
 #include "kobuki_msgs/BumperEvent.h"
 class Stopper
 {
-	public://Tunable parameters
+public://Tunable parameters
 
-        const static double FORWARD_SPEED_MPS = 0.2;
-        const static double MIN_SCAN_ANGLE_RAD = -0.2 +M_PI;
-        const static double MAX_SCAN_ANGLE_RAD = 0.2 + M_PI;
-        const static float  MIN_PROXIMITY_RANGE_M = 1.5;	//Should be smaller than sensor_msgs::LaserScan::range_max
-	    Stopper();
-		void startMoving();
+  const static double FORWARD_SPEED_MPS = 0.2;
+  const static double MIN_SCAN_ANGLE_RAD = -0.2 +M_PI;
+  const static double MAX_SCAN_ANGLE_RAD = 0.2 + M_PI;
+  const static float  MIN_PROXIMITY_RANGE_M = 1.5;	//Should be smaller than sensor_msgs::LaserScan::range_max
+  Stopper();
+  void startMoving();
 
-	private:  	
-  	
-		ros::NodeHandle	node;
-		ros::Publisher commandPub;	//	Publisher to the robot's velocity command topic	
-		ros::Subscriber	laserSub;	// Subscriber to the robot's laser scan topic
-        ros::Subscriber markerId;   // Subscriber to the detect_markers topic including the founded aruco-markers
-        ros::Subscriber bumper;     // Subscriber to the robot's bumber event topic
+private:
 
-		bool keepMoving; //Indicates wether the robot should continue moving
-        int  nextId;
-		void moveForward();
-		void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
-        void rotate();
-        void markerCallback(const detect_marker::Markers::ConstPtr& marker_msg);
-        void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& bumper_msg);
+  ros::Subscriber m_markerSub;
+  ros::NodeHandle	node;
+  ros::Publisher commandPub;	//	Publisher to the robot's velocity command topic
+  ros::Subscriber	laserSub;	// Subscriber to the robot's laser scan topic
+  ros::Subscriber markerId;   // Subscriber to the detect_markers topic including the founded aruco-markers
+  ros::Subscriber bumper;     // Subscriber to the robot's bumber event topic
+
+  bool keepMoving; //Indicates wether the robot should continue moving
+  int  nextId;
+  float m_angularVelocity;
+
+  void moveForward();
+  void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+  void rotate();
+  void markerCallback(const detect_marker::Markers::ConstPtr& marker_msg);
+  void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& bumper_msg);
+
+  // *****
+  void markerDetectCallback(const detect_marker::Markers::ConstPtr& marker_msg);
+  void rotateTurtlebot();
 };
