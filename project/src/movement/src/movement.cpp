4fc632c3ad74387f6c14f5554f88978a36032600
Sivris <ga73kec@hpc03.clients.eikon.tum.de>
2016-01-27 11:14:53 +0100

    working version of movement

--- project/src/movement/src/movement.cpp
+++ project/src/movement/src/movement.cpp
@@ -26,31 +26,8 @@ Mover::Mover()
     //  imageSub = node.subscribe("/camera/rgb/image_rect_color", 10, &Mover::rgbCallback, this);
     getLocationSub = node.subscribe("/markerinfo", 10, &Mover::getLocationCallback, this);
     targetFinishedSub = node.subscribe("targetFinishedTopic", 10, &Mover::targetFinishedCallback, this);
-    int nbRanges = ceil(360 / ANGLE_PRECISION);
-    m_ranges = new double[nbRanges];
-    for (int i=0 ; i < nbRanges ; i++)
-        m_ranges[i] = nan("");
 }
 
-//angle in rad
-void Mover::rotateTurtle(double angle)
-{
-    double angVel = 0.25;
-    double target_time = angle/angVel;
-    geometry_msgs::Twist base_cmd;
-    base_cmd.linear.y = 0;
-    base_cmd.linear.x = 0;
-    base_cmd.angular.x = base_cmd.angular.y = 0;
-    ros::Rate rate(100);
-    while( ros::Time::now().toSec() <= (ros::Time::now().toSec() + target_time)  )
-    {
-        base_cmd.angular.z = angVel;
-        commandPub.publish(base_cmd);
-        rate.sleep();
-    }
-    base_cmd.angular.z = 0;
-    commandPub.publish(base_cmd);
-}
 
 void Mover::driveForwardOdom(double distance)
 {
@@ -174,8 +151,7 @@ void Mover::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
     float closestRange = scan->ranges[minIndex];
     //save distance between robot and marker (which is in the center)
     if(scan->ranges[0]<=10 && scan->ranges[0]!=INFINITY){m_distanceToMarker = scan->ranges[0];}
-    m_distanceToMarker = scan->ranges[(maxIndex-minIndex)/2];
-    //ROS_INFO("Distance to marker %f !", m_distanceToMarker);
+    ROS_INFO("Distance to marker %f !", m_distanceToMarker);
     for(int currIndex = minIndex+1; currIndex<=maxIndex; currIndex++)
     {
         if(scan->ranges[currIndex] < closestRange)
@@ -183,10 +159,8 @@ void Mover::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
             closestRange =	scan->ranges[currIndex];
         }
     }
-    ROS_INFO("closest range = %f", closestRange);
-
-    if(closestRange<MIN_PROXIMITY_RANGE_M)
-
+    //ROS_INFO("closest range = %f", closestRange);
+    if(closestRange < MIN_PROXIMITY_RANGE_M)
     {
         m_keepMoving=false;
         /*  //check if you have a target we are heading for
@@ -194,13 +168,13 @@ void Mover::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
         {
           m_gotTarget = false;
           m_reachedTarget = true;
-          rotateTurtle(180);
+          rotateOdom(180);
           driveForwardOdom(0.75);
         }
        else*/
         //rotate only if not approaching target
         ROS_INFO("Obstacle in %f Turn 25 degrees!", closestRange);
-        rotateTurtle(25.0*M_PI/180);
+        rotateOdom(25.0);
 
     }
     else
@@ -233,14 +207,14 @@ void Mover::bumperSubCallback(const kobuki_msgs::BumperEvent::ConstPtr& bumperSu
         commandPub.publish(base_cmd);//drive backwards
         ros::Duration(0.5).sleep(); // sleep for half a second
         m_keepMoving=false;
-        rotateTurtle(45*M_PI/180);
+        rotateOdom(45);
         ros::spinOnce();
     }
 }
 
 void Mover::approachMarker(int param_marker_id)
 {
-    //ros::Rate rateX(20);
+    ros::Rate rateX(20);
     geometry_msgs::Twist vel_msg;
     //save old distance to check if object is valid
     static float f_distance_old = m_distanceToMarker; // save
@@ -276,19 +250,18 @@ void Mover::approachMarker(int param_marker_id)
             vel_msg.angular.z = 0;
             vel_msg.linear.x = 0;
             commandPub.publish(vel_msg);
-            rotateTurtle(358*M_PI/180);
+            rotateOdom(358);
             driveForwardOdom(0.75);
         }
         ROS_INFO("Target Reached!!! The Marker is %d", m_searchMarker++);
         ROS_INFO("Searching next target: %d , distance is %f", m_searchMarker, m_distanceToMarker);
         m_gotTarget = false;
         m_reachedTarget = true;
-        rotateTurtle(1);
+        rotateOdom(180);
         ROS_INFO("Turned 180 degrees");
     }
-      ros::spinOnce();
-    //rateX.sleep();
-
+    rateX.sleep();
+    ros::spinOnce();
 }
 
 void Mover::getLocationCallback(const detect_marker::MarkersInfos::ConstPtr &marker_msg)
@@ -346,9 +319,9 @@ void Mover::getLocationCallback(const detect_marker::MarkersInfos::ConstPtr &mar
 void Mover::targetFinishedCallback(const std_msgs::EmptyConstPtr empty)
 {
     m_gotTarget = false;
-    rotateTurtle(1);
+    rotateOdom(180);
     driveForwardOdom(0.5);
-    rotateTurtle(358*M_PI/180);
+    rotateOdom(358);
 }
 void Mover::moveRandomly()
 {
@@ -356,7 +329,7 @@ void Mover::moveRandomly()
     double randDist = 1.0*((double) rand() / (RAND_MAX));
     double randAngle = 90.0*((double) rand() / (RAND_MAX));
     // ROS_INFO("Random Angle = %f", randAngle);
-    rotateTurtle(randAngle * M_PI/180);
+    rotateOdom(randAngle);
     //ROS_INFO("Random Distance = %f", randDist);
     driveForwardOdom(randDist);
     //ROS_INFO("Found nothing");
@@ -382,7 +355,7 @@ void Mover::startMoving()
     }
     while(!m_keepMoving)
     {
-        rotateTurtle(30*M_PI/180);
+        rotateOdom(30);
         ros::spinOnce();
     }
 }
