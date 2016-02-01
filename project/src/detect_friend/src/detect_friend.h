c9c907e42f48fdd0076762077964578b165e8ed1
Alexandra Parasidi <ga59muq@mytum.de>
2016-02-01 23:17:11 +0000

    Publish center, distance, timestamp for friend

--- project/src/detect_friend/src/detect_friend.h
+++ project/src/detect_friend/src/detect_friend.h
@@ -9,15 +9,24 @@
 #include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/imgproc/imgproc.hpp>
 #include "friendmatcher.h"
+#include "detect_friend/Friend_id.h"
+#include "detect_friend/FriendsInfos.h"
 
 class DetectFriend
 {
     public:
         DetectFriend(ros::NodeHandle& nodeHandle);
-	    void Identification();
+	void Identification();
         const static double min_score =0.7;
+        static const double FRIEND_REF_DIST = 480 * 0.2 / 0.175;
+        static const double COIN_HEIGHT = 0.1785;
+	static const double MUSHROOM_HEIGHT=0.1785;
+	static const double STAR_HEIGHT=0.1865;
     private:
         ros::NodeHandle& m_nodeHandle;
+        detect_friend::Friend_id publish_infos_of_friend(const cv::Mat& img, int i, cv::Rect rectangle);
+	static bool ComputeLinesIntersection(cv::Point linePoints1[2], cv::Point linePoints2[2], cv::Point *isectPoint);
+        static bool ComputeQuadrilateralCenter(cv::Point points[4], cv::Point *centerPoint);
         ros::Subscriber m_cameraSub;
         ros::Publisher m_friend_idPub;
         cv::Mat star_image;
